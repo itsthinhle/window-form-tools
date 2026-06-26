cb5a0dd94570d1e563caa6b0d94bfd646d0e3696
Tat Thinh Le <letatthinh.1997@gmail.com>
2026-06-26 15:43:04 -0400

    Update controls' style and add a label to indicate the format of output videos

--- Tools/Tools/ControlLibrary/Layouts/AppTableLayout.cs
+++ Tools/Tools/ControlLibrary/Layouts/AppTableLayout.cs
@@ -7,6 +7,5 @@ internal class AppTableLayout : TableLayoutPanel
         ColumnCount = 2;
         Margin = new Padding(0);
         RowCount = 2;
-        Dock = DockStyle.Fill;
     }
 }
--- Tools/Tools/VideoTrimmingForm.Designer.cs
+++ Tools/Tools/VideoTrimmingForm.Designer.cs
@@ -53,14 +53,14 @@ partial class VideoTrimmingForm
         appGroupBox2 = new AppGroupBox();
         appTableLayout3 = new AppTableLayout();
         appPanel4 = new AppPanel();
-        appPanel5 = new AppPanel();
+        _buttonGoToOutputFolder = new AppIconButton();
         _buttonBrowseOutputFolder = new AppIconButton();
-        _buttonOpenOutputFolder = new AppIconButton();
         _textBoxOutputFolderPath = new AppTextBox();
         appLabel3 = new AppLabel();
         appPanel3 = new AppPanel();
         _textBoxOutputVideoName = new AppTextBox();
         appLabel4 = new AppLabel();
+        appNormalText3 = new AppNormalText();
         appGroupBox3 = new AppGroupBox();
         _checkBoxShouldDeleteSourceVideoAfterTrimming = new AppCheckBox();
         appGroupBox4 = new AppGroupBox();
@@ -75,7 +75,6 @@ partial class VideoTrimmingForm
         appGroupBox2.SuspendLayout();
         appTableLayout3.SuspendLayout();
         appPanel4.SuspendLayout();
-        appPanel5.SuspendLayout();
         appPanel3.SuspendLayout();
         appGroupBox3.SuspendLayout();
         appGroupBox4.SuspendLayout();
@@ -141,6 +140,7 @@ partial class VideoTrimmingForm
         // 
         // appPanel1
         // 
+        appPanel1.BackColor = Color.Transparent;
         appPanel1.Controls.Add(_buttonBrowseVideo);
         appPanel1.Controls.Add(_textBoxVideoPath);
         appPanel1.Controls.Add(appLabel1);
@@ -227,6 +227,7 @@ partial class VideoTrimmingForm
         // 
         // appPanel2
         // 
+        appPanel2.BackColor = Color.Transparent;
         appPanel2.Controls.Add(_buttonClearTimeRanges);
         appPanel2.Controls.Add(_richTextBoxTimeSegments);
         appPanel2.Controls.Add(_buttonStart);
@@ -295,63 +296,74 @@ partial class VideoTrimmingForm
         appTableLayout2.Margin = new Padding(0);
         appTableLayout2.Name = "appTableLayout2";
         appTableLayout2.RowCount = 2;
-        appTableLayout2.RowStyles.Add(new RowStyle(SizeType.Absolute, 131F));
+        appTableLayout2.RowStyles.Add(new RowStyle());
         appTableLayout2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
         appTableLayout2.Size = new Size(758, 506);
         appTableLayout2.TabIndex = 1;
         // 
         // appGroupBox2
         // 
+        appGroupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
         appGroupBox2.Controls.Add(appTableLayout3);
-        appGroupBox2.Dock = DockStyle.Fill;
         appGroupBox2.Font = new Font("Segoe UI Semibold", 13F, FontStyle.Bold);
         appGroupBox2.Location = new Point(9, 0);
         appGroupBox2.Margin = new Padding(9, 0, 0, 9);
         appGroupBox2.Name = "appGroupBox2";
         appGroupBox2.Padding = new Padding(9);
-        appGroupBox2.Size = new Size(749, 122);
+        appGroupBox2.Size = new Size(749, 165);
         appGroupBox2.TabIndex = 0;
         appGroupBox2.TabStop = false;
         appGroupBox2.Text = "Output";
         // 
         // appTableLayout3
         // 
+        appTableLayout3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
         appTableLayout3.ColumnCount = 1;
         appTableLayout3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-        appTableLayout3.Controls.Add(appPanel4, 0, 1);
+        appTableLayout3.Controls.Add(appPanel4, 0, 2);
         appTableLayout3.Controls.Add(appPanel3, 0, 0);
-        appTableLayout3.Dock = DockStyle.Fill;
+        appTableLayout3.Controls.Add(appNormalText3, 0, 1);
         appTableLayout3.Location = new Point(9, 33);
         appTableLayout3.Margin = new Padding(0, 0, 0, 9);
         appTableLayout3.Name = "appTableLayout3";
-        appTableLayout3.RowCount = 2;
+        appTableLayout3.RowCount = 3;
         appTableLayout3.RowStyles.Add(new RowStyle());
-        appTableLayout3.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-        appTableLayout3.Size = new Size(731, 80);
+        appTableLayout3.RowStyles.Add(new RowStyle());
+        appTableLayout3.RowStyles.Add(new RowStyle());
+        appTableLayout3.Size = new Size(731, 123);
         appTableLayout3.TabIndex = 0;
         // 
         // appPanel4
         // 
-        appPanel4.Controls.Add(appPanel5);
+        appPanel4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        appPanel4.BackColor = Color.Transparent;
+        appPanel4.Controls.Add(_buttonGoToOutputFolder);
+        appPanel4.Controls.Add(_buttonBrowseOutputFolder);
         appPanel4.Controls.Add(_textBoxOutputFolderPath);
         appPanel4.Controls.Add(appLabel3);
-        appPanel4.Dock = DockStyle.Top;
-        appPanel4.Location = new Point(0, 49);
+        appPanel4.Location = new Point(0, 92);
         appPanel4.Margin = new Padding(0);
         appPanel4.Name = "appPanel4";
         appPanel4.Size = new Size(731, 31);
-        appPanel4.TabIndex = 1;
+        appPanel4.TabIndex = 2;
         // 
-        // appPanel5
+        // _buttonGoToOutputFolder
         // 
-        appPanel5.Anchor = AnchorStyles.Right;
-        appPanel5.Controls.Add(_buttonBrowseOutputFolder);
-        appPanel5.Controls.Add(_buttonOpenOutputFolder);
-        appPanel5.Location = new Point(660, 0);
-        appPanel5.Margin = new Padding(0);
-        appPanel5.Name = "appPanel5";
-        appPanel5.Size = new Size(71, 31);
-        appPanel5.TabIndex = 4;
+        _buttonGoToOutputFolder.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+        _buttonGoToOutputFolder.BackColor = Color.Transparent;
+        _buttonGoToOutputFolder.BackgroundImage = (Image)resources.GetObject("_buttonGoToOutputFolder.BackgroundImage");
+        _buttonGoToOutputFolder.BackgroundImageLayout = ImageLayout.Stretch;
+        _buttonGoToOutputFolder.FlatAppearance.BorderSize = 0;
+        _buttonGoToOutputFolder.FlatAppearance.MouseDownBackColor = Color.Transparent;
+        _buttonGoToOutputFolder.FlatAppearance.MouseOverBackColor = Color.Transparent;
+        _buttonGoToOutputFolder.FlatStyle = FlatStyle.Flat;
+        _buttonGoToOutputFolder.Location = new Point(706, 3);
+        _buttonGoToOutputFolder.Margin = new Padding(9, 0, 0, 0);
+        _buttonGoToOutputFolder.Name = "_buttonGoToOutputFolder";
+        _buttonGoToOutputFolder.Size = new Size(25, 25);
+        _buttonGoToOutputFolder.TabIndex = 5;
+        _buttonGoToOutputFolder.UseVisualStyleBackColor = false;
+        _buttonGoToOutputFolder.Click += ButtonGoToOutputFolder_Click;
         // 
         // _buttonBrowseOutputFolder
         // 
@@ -363,41 +375,23 @@ partial class VideoTrimmingForm
         _buttonBrowseOutputFolder.FlatAppearance.MouseDownBackColor = Color.Transparent;
         _buttonBrowseOutputFolder.FlatAppearance.MouseOverBackColor = Color.Transparent;
         _buttonBrowseOutputFolder.FlatStyle = FlatStyle.Flat;
-        _buttonBrowseOutputFolder.Location = new Point(0, 0);
-        _buttonBrowseOutputFolder.Margin = new Padding(0, 0, 9, 0);
+        _buttonBrowseOutputFolder.Location = new Point(666, 0);
+        _buttonBrowseOutputFolder.Margin = new Padding(18, 0, 0, 0);
         _buttonBrowseOutputFolder.Name = "_buttonBrowseOutputFolder";
         _buttonBrowseOutputFolder.Size = new Size(31, 31);
-        _buttonBrowseOutputFolder.TabIndex = 2;
+        _buttonBrowseOutputFolder.TabIndex = 3;
         _buttonBrowseOutputFolder.UseVisualStyleBackColor = false;
         _buttonBrowseOutputFolder.Click += ButtonBrowseOutputFolder_Click;
-        // 
-        // _buttonOpenOutputFolder
-        // 
-        _buttonOpenOutputFolder.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-        _buttonOpenOutputFolder.BackColor = Color.Transparent;
-        _buttonOpenOutputFolder.BackgroundImage = (Image)resources.GetObject("_buttonOpenOutputFolder.BackgroundImage");
-        _buttonOpenOutputFolder.BackgroundImageLayout = ImageLayout.Stretch;
-        _buttonOpenOutputFolder.FlatAppearance.BorderSize = 0;
-        _buttonOpenOutputFolder.FlatAppearance.MouseDownBackColor = Color.Transparent;
-        _buttonOpenOutputFolder.FlatAppearance.MouseOverBackColor = Color.Transparent;
-        _buttonOpenOutputFolder.FlatStyle = FlatStyle.Flat;
-        _buttonOpenOutputFolder.Location = new Point(40, 0);
-        _buttonOpenOutputFolder.Margin = new Padding(0);
-        _buttonOpenOutputFolder.Name = "_buttonOpenOutputFolder";
-        _buttonOpenOutputFolder.Size = new Size(31, 31);
-        _buttonOpenOutputFolder.TabIndex = 3;
-        _buttonOpenOutputFolder.UseVisualStyleBackColor = false;
-        _buttonOpenOutputFolder.Click += ButtonOpenOutputFolder_Click;
         // 
         // _textBoxOutputFolderPath
         // 
         _textBoxOutputFolderPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
         _textBoxOutputFolderPath.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
         _textBoxOutputFolderPath.Location = new Point(192, 0);
-        _textBoxOutputFolderPath.Margin = new Padding(0, 0, 18, 0);
+        _textBoxOutputFolderPath.Margin = new Padding(0);
         _textBoxOutputFolderPath.Name = "_textBoxOutputFolderPath";
-        _textBoxOutputFolderPath.Size = new Size(450, 31);
-        _textBoxOutputFolderPath.TabIndex = 1;
+        _textBoxOutputFolderPath.Size = new Size(456, 31);
+        _textBoxOutputFolderPath.TabIndex = 4;
         // 
         // appLabel3
         // 
@@ -412,6 +406,7 @@ partial class VideoTrimmingForm
         // 
         // appPanel3
         // 
+        appPanel3.BackColor = Color.Transparent;
         appPanel3.Controls.Add(_textBoxOutputVideoName);
         appPanel3.Controls.Add(appLabel4);
         appPanel3.Dock = DockStyle.Top;
@@ -441,16 +436,27 @@ partial class VideoTrimmingForm
         appLabel4.TabIndex = 2;
         appLabel4.Text = "Output video name";
         // 
+        // appNormalText3
+        // 
+        appNormalText3.AutoSize = true;
+        appNormalText3.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        appNormalText3.Location = new Point(0, 49);
+        appNormalText3.Margin = new Padding(0, 0, 0, 18);
+        appNormalText3.Name = "appNormalText3";
+        appNormalText3.Size = new Size(553, 25);
+        appNormalText3.TabIndex = 1;
+        appNormalText3.Text = "Output format: <video name>_<index>_<file description>.extension";
+        // 
         // appGroupBox3
         // 
         appGroupBox3.Controls.Add(_checkBoxShouldDeleteSourceVideoAfterTrimming);
         appGroupBox3.Dock = DockStyle.Fill;
         appGroupBox3.Font = new Font("Segoe UI Semibold", 13F, FontStyle.Bold);
-        appGroupBox3.Location = new Point(9, 131);
+        appGroupBox3.Location = new Point(9, 174);
         appGroupBox3.Margin = new Padding(9, 0, 0, 9);
         appGroupBox3.Name = "appGroupBox3";
         appGroupBox3.Padding = new Padding(9);
-        appGroupBox3.Size = new Size(749, 366);
+        appGroupBox3.Size = new Size(749, 323);
         appGroupBox3.TabIndex = 1;
         appGroupBox3.TabStop = false;
         appGroupBox3.Text = "AppSettings";
@@ -533,9 +539,9 @@ partial class VideoTrimmingForm
         appTableLayout2.ResumeLayout(false);
         appGroupBox2.ResumeLayout(false);
         appTableLayout3.ResumeLayout(false);
+        appTableLayout3.PerformLayout();
         appPanel4.ResumeLayout(false);
         appPanel4.PerformLayout();
-        appPanel5.ResumeLayout(false);
         appPanel3.ResumeLayout(false);
         appPanel3.PerformLayout();
         appGroupBox3.ResumeLayout(false);
@@ -561,20 +567,20 @@ partial class VideoTrimmingForm
     private AppRichTextBox _richTextBoxTimeSegments;
     private AppTableLayout appTableLayout2;
     private AppGroupBox appGroupBox2;
+    private AppGroupBox appGroupBox3;
+    private AppGroupBox appGroupBox4;
+    private AppRichTextBox _richTextBoxLogs;
+    private AppIconButton _buttonClearTimeRanges;
+    private AppCheckBox _checkBoxShouldDeleteSourceVideoAfterTrimming;
+    private AppSecondaryButton _buttonSaveFormData;
     private AppTableLayout appTableLayout3;
     private AppPanel appPanel4;
-    private AppIconButton _buttonBrowseOutputFolder;
-    private AppTextBox _textBoxOutputFolderPath;
     private AppLabel appLabel3;
     private AppPanel appPanel3;
     private AppTextBox _textBoxOutputVideoName;
     private AppLabel appLabel4;
-    private AppGroupBox appGroupBox3;
-    private AppIconButton _buttonOpenOutputFolder;
-    private AppGroupBox appGroupBox4;
-    private AppRichTextBox _richTextBoxLogs;
-    private AppPanel appPanel5;
-    private AppIconButton _buttonClearTimeRanges;
-    private AppCheckBox _checkBoxShouldDeleteSourceVideoAfterTrimming;
-    private AppSecondaryButton _buttonSaveFormData;
+    private AppNormalText appNormalText3;
+    private AppIconButton _buttonBrowseOutputFolder;
+    private AppTextBox _textBoxOutputFolderPath;
+    private AppIconButton _buttonGoToOutputFolder;
 }
